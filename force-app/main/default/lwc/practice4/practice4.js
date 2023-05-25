5cc8a01d56ae9d0555f4f5136ec32307cdb3ef3b
Ali Alzuhairi <hod.20088@gmail.com>
2023-05-25 11:21:18 -0400

    Day 59 Practice5 was homework in class

--- force-app/main/default/lwc/practice4/practice4.js
+++ force-app/main/default/lwc/practice4/practice4.js
@@ -2,23 +2,49 @@ import { LightningElement } from 'lwc';
 
 export default class Practice4 extends LightningElement {
 
-    num1 = 0 ; 
-    num2 = 0;
+    num1 = 0.00; 
+    num2 = 0.00;
+    value1='';
+    value2='';
     
     get additionResult() {
         return this.num1 + this.num2; 
+    }
+    get subtractionResult(){
+        return this.num1 - this.num2;
+    }
+
+    get multiplicationResult(){
+        return this.num1 * this.num2;
+    }
+
+    get divisionResult(){
+        return this.num1 / this.num2;
     }
 
 
     handleNum1Change(event) {
         // set the value of num1 to what user typed
-        this.num1 =  parseFloat( event.target.value ) ; 
+        this.num1 =  parseFloat( event.target.value ) ;
+        this.value1= event.target.value;
+         
     }
 
     handleNum2Change(event) {
         // set the value of num2 to what user typed
         this.num2 = parseFloat( event.target.value ); 
+        this.value2= event.target.value;
+        
     }
+
+    handleReset(){
+        this.num1 = 0;
+        this.num2 = 0;
+        this.value1 = '';
+        this.value2 = '';
+        
+    }
+    
 
 
 }
