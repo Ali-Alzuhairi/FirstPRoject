1ce3c93eb7a70ebfce00bfc1e5e97c6c1fa84db7
Ali Alzuhairi <hod.20088@gmail.com>
2023-06-02 22:33:49 -0400

    Day62:lightning record for and Toast practice22,23

---
+++ force-app/main/default/lwc/practice22/practice22.js
@@ -0,0 +1,32 @@
+import { LightningElement } from "lwc";
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
+import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import PHONE_FIELD from "@salesforce/schema/Account.Phone";
+// start typing this impt to get help 
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+export default class practice22 extends LightningElement {
+
+    objectApiName = ACCOUNT_OBJECT ; 
+    fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD, PHONE_FIELD];
+
+    handleSuccess(event) {
+
+        console.log(event.detail);
+        console.log(event.detail.id);
+        console.log(event.detail.fields.AnnualRevenue);
+        
+        const showToastEvent = new ShowToastEvent({
+            title: 'Successfully Created',
+            message: 'You have created account successfully',
+            variant: 'success'
+        }); 
+
+        this.dispatchEvent(showToastEvent);
+
+
+    } 
+}
